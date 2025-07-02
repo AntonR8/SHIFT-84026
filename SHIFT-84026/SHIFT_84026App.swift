586e12523171bfcec64036bc9e441694575da5ba
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2025-07-02 11:07:37 +0300

    Initial Commit

---
+++ SHIFT-84026/SHIFT_84026App.swift
@@ -0,0 +1,17 @@
+//
+//  SHIFT_84026App.swift
+//  SHIFT-84026
+//
+//  Created by Антон Разгуляев on 02.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SHIFT_84026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
